1d602c6f546e859184867938948cfd888cae370f
Mathew Rupp <admin@rikairchy.net>
2019-04-02 18:56:02 -0400

    adding env setup script

---
+++ setenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export vcenter_password=
+export vcenter_username=
+export vcenter_server=
+export esx_host=
+export redhat_sub_user=
+export redhat_sub_pass=
+export login_password=
+export cluster=
+export datastore=
+export datacenter=
+export iso_datastore=
+export winrm_password=
+
